0ce53855319d72aa2e1b5af91e39f822a0c8b624
Jimin Park <pjm950@gmail.com>
2015-05-11 22:58:57 +0900

    Added command mechanisms and added ping plugin

---
+++ plugins/ping.js
@@ -0,0 +1,17 @@
+function PingPlugin(){}
+
+PingPlugin.prototype.bot = null;
+
+PingPlugin.prototype.init = function PingPlugin$init(bot){
+	this.bot = bot;
+
+	bot.addCommand("ping", "핑/pda/vld".split('/'),
+		"@: 사용자에게 퐁을 때려줍니다.", function(msg, ctx, cmd, args){
+		bot.reply(ctx, msg.nick+", pong!");
+	});
+};
+
+PingPlugin.prototype.destroy = function PingPlugin$destroy(){
+};
+
+module.exports = new PingPlugin;
